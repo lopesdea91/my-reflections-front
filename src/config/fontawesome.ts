5e01d243ce300ad7e6e11bc74d988efe71a5ebcb
lopesdea91 <lopesdea91@gmail.com>
2022-10-15 19:17:45 -0300

    install fontAwesome

---
+++ src/config/fontawesome.ts
@@ -0,0 +1,21 @@
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faUser, faHome, faHashtag, faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faBell } from '@fortawesome/free-regular-svg-icons'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(
+    faUser, faHome, faHashtag, faBookmark,
+    faTwitter,
+    faBell
+)
+
+export {
+    FontAwesomeIcon
+}
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import App from './App.vue'
 
 import { pinia } from './config/pinia'
 import { router } from './config/router'
+import { FontAwesomeIcon } from './config/fontawesome'
 
 const app = createApp(App)
+app.component('Icon', FontAwesomeIcon)
 app.use(pinia)
 app.use(router)
 app.mount('#app')
