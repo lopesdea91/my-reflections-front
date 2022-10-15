3bfecce8fe61062eb72d9ae18b9b6025bb781670
lopesdea91 <lopesdea91@gmail.com>
2022-10-15 13:05:51 -0300

    config vue-router

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import './assets/css/app.scss'
 import App from './App.vue'
 
 import { pinia } from './config/pinia'
+import { router } from './config/router'
 
 const app = createApp(App)
 app.use(pinia)
+app.use(router)
 app.mount('#app')
